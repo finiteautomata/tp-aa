a2e67f925f7f27dcde7ef32cab6b066abf30e618
Juan Manuel Pérez <jmperez.85@gmail.com>
2016-09-14 02:11:25 -0300

    PEP8

--- dataframe_builder.py
+++ dataframe_builder.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+"""Clase que se encarga de construir el dataframe en base a los ham y spam."""
 # Aprendizaje Automatico - DC, FCEN, UBA
 # Segundo cuatrimestre 2016
 
@@ -6,32 +7,47 @@ import pandas as pd
 
 
 class DataFrameBuilder(object):
+    """
+    Construye un dataframe en base al dataset de spam y ham.
+
+    Uso:
+
+    > ham_txt = json.load(open('data/ham_dev.json'))
+    > spam_txt = json.load(open('data/spam_dev.json'))
+    > builder = DataFrameBuilder(ham=ham_txt, spam=spam_txt)
+    > df = builder.build()
+    """
+
     def __init__(self, spam, ham):
+        """Constructor."""
         self.spam = spam
         self.ham = ham
+        self.list_of_attributes = []
 
     def build(self):
+        u"""
+        Construye el dataframe.
+
+        Devuelve el dataframe ya construído
+        """
         klass = ['spam'] * len(self.spam) + ['ham'] * len(self.ham)
 
         self.df = pd.DataFrame({'text': self.spam + self.ham, 'class': klass})
-
-        self.list_of_attributes = []
 
         self.add_attribute(len, 'len')
         self.add_attribute(lambda t: t.count(' '), 'spaces')
         self.add_word_attribute("<html>", "has_html")
         self.add_word_attribute("Original Message", "has_original_message")
-        self.add_word_attribute("free", lower=True)
-        self.add_word_attribute("cc:", lower=True)
 
-        for word in ["gif", "help", "http", "dollar", "million", "|"]:
-            self.add_word_attribute(word)
+        for word in ["free", "cc:", "gif", "help",
+                     "http", "dollar", "million", "|"]:
+            self.add_word_attribute(word, lower=True)
 
         return self.df
 
     def add_attribute(self, fun, column_name):
         """
-        Agrega columna al dataframe df que consiste en aplicar fun a df.text
+        Agrega columna al dataframe df que consiste en aplicar fun a df.text.
 
         Atributos:
 
@@ -40,14 +56,12 @@ class DataFrameBuilder(object):
         - column_name: Opcional. Nombre de la columna a crear en el dataframe
         - lower: Busco case insensitive (False por defecto)
         """
-
         self.list_of_attributes.append(column_name)
         self.df[column_name] = map(fun, self.df.text)
 
     def add_word_attribute(self, word, column_name=None, lower=False):
         """
-        Agrega una columna al dataframe df que consiste en ver
-        si word esta presente en la columna df.text
+        Agrega columna al dataframe df que cuenta ocurrencias de word.
 
         Atributos:
 
@@ -56,7 +70,6 @@ class DataFrameBuilder(object):
         - column_name: Opcional. Nombre de la columna a crear en el dataframe
         - lower: Busco case insensitive (False por defecto)
         """
-
         column_name = column_name or ("has_" + word)
 
         def fun(text):
