c7a10716b3263c8c851c4274c99862ccde9338e7
loopokus <mariela.raj@gmail.com>
2016-09-25 13:56:20 -0300

    Arreglo problema en path

--- config.py
+++ config.py
@@ -11,8 +11,8 @@ def project_dir():
 
 
 root_dir = project_dir()
-cache_dir = os.path.join(root_dir, "cache/")
-data_dir = os.path.join(root_dir, "data/")
+cache_dir = os.path.join(root_dir, "cache")
+data_dir = os.path.join(root_dir, "data")
 
 ham_dev_path = os.path.join(data_dir, "ham_dev.json")
 spam_dev_path = os.path.join(data_dir, "spam_dev.json")
