a2ce3e5319139670e70385929288bb5210ec84ec
Juan Manuel Perez <jmperez.85@gmail.com>
2016-11-14 14:56:59 -0300

    Agrego transformers para los headers.

--- transformers/__init__.py
+++ transformers/__init__.py
@@ -4,6 +4,7 @@ from sklearn.pipeline import make_union
 from sklearn_pandas import DataFrameMapper
 from .base import BaseTransformer
 from .payload import SpaceTransformer, LenTransformer, AddWordsTransformer
+from .header import ContentTypeTransformer, ParticipantsTransformer
 
 options = {
     'max_features': 100,
@@ -20,6 +21,13 @@ payload_transformer = make_union(
     TfidfVectorizer(**options),
 )
 
+parsed_mail_transformer = make_union(
+    ContentTypeTransformer(),
+    ParticipantsTransformer(),
+)
+
+
 transformer = DataFrameMapper([
-    ('payload', payload_transformer)
+    ('payload', payload_transformer),
+    ('parsed_emails', parsed_mail_transformer),
 ])
--- transformers/header.py
+++ transformers/header.py
@@ -1,25 +1,40 @@
+#! coding: utf-8
 """Transformers del header."""
-import email
 import re
+import numpy as np
 from . import BaseTransformer
 
 
-class AddHeaderAttributesTransformer(BaseTransformer):
+class ContentTypeTransformer(BaseTransformer):
+    """Agrega atributos basados en content/type"""
+
+    def transform(self, mails):
+        """Agrego todas las columnas relacionadas a content type."""
+        content_type = [m.get_content_type() for m in mails]
+
+        types = [
+            'multipart/mixed', 'text/html', 'multipart/alternative',
+            'text/plain', 'multipart/related', 'multipart/report',
+            'text/plain charset="us-ascii"', 'text/html charset=iso-8859-1',
+            'application/vnd.ms-excel', 'message/rfc822', 'text/enriched',
+            'text/richtext', 'image/pjpeg', 'application/msword',
+            'application/octet-stream']
+
+        new_columns = []
+
+        for ctype in types:
+            new_columns.append([ctype in t for t in content_type])
+
+        return np.array(new_columns).transpose()
+
+
+class ParticipantsTransformer(BaseTransformer):
     """Agrega atributos del header."""
 
-    def transform(self, df):
+    def transform(self, mails):
         u"""Aplico transformación."""
-        parser = email.parser.Parser()
-
-        parsed_emails = df['text'].apply(
-            lambda t: parser.parsestr(t.encode('utf-8')))
-
-        self.add_content_type_columns(df, parsed_emails)
-
-        receivers = parsed_emails.apply(
-            lambda p: p.get_all("To") or p.get_all("to") or [])
-        sender = parsed_emails.apply(
-            lambda p: p.get_all("From") or p.get_all("from") or [])
+        receivers = [p.get_all("To") or p.get_all("to") or [] for p in mails]
+        sender = [p.get_all("From") or p.get_all("from") or [] for p in mails]
 
         def join_mails(t):
             return ";".join(t)
@@ -31,32 +46,10 @@ class AddHeaderAttributesTransformer(BaseTransformer):
             except UnicodeDecodeError:
                 return False
 
-        to_text = receivers.apply(join_mails)
-        from_text = df['from'] = sender.apply(join_mails)
+        to_text = map(join_mails, receivers)
+        from_text = map(join_mails, sender)
 
-        df['number_of_receivers'] = to_text.apply(
-            lambda t: len(re.findall(r'<.*>', t))
-        )
+        no_receivers = [len(re.findall(r'<.*>', t)) for t in to_text]
+        from_non_ascii = [not is_ascii(from_list) for from_list in from_text]
 
-        df['from_non_ascii'] = from_text.apply(
-            lambda from_list: not is_ascii(from_list)
-        )
-
-        return df
-
-    def add_content_type_columns(self, df, parsed_emails):
-        """Agrego todas las columnas relacionadas a content type."""
-        content_type = parsed_emails.apply(
-            lambda t: t.get_content_type())
-
-        types = [
-            'multipart/mixed', 'text/html', 'multipart/alternative',
-            'text/plain', 'multipart/related', 'multipart/report',
-            'text/plain charset="us-ascii"', 'text/html charset=iso-8859-1',
-            'application/vnd.ms-excel', 'message/rfc822', 'text/enriched',
-            'text/richtext', 'image/pjpeg', 'application/msword',
-            'application/octet-stream']
-
-        for ctype in types:
-            df[ctype] = content_type.apply(
-                lambda t: ctype in t)
+        return np.array([no_receivers, from_non_ascii]).transpose()
